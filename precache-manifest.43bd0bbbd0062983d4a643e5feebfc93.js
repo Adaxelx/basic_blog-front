de28b4acaf1afa2f07a15e9435d1bb591d66d035
Adrian Mostowski <adaxelx@o2.pl>
2020-06-27 12:24:20 +0200

    Updates

---
+++ precache-manifest.43bd0bbbd0062983d4a643e5feebfc93.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "51f88b0d86f10a457d5e8428a8c89c6c",
+    "url": "/basic_blog-front/index.html"
+  },
+  {
+    "revision": "0929379f8afa5591b663",
+    "url": "/basic_blog-front/static/css/main.cba5ae94.chunk.css"
+  },
+  {
+    "revision": "f0e970ce4ed021b363f9",
+    "url": "/basic_blog-front/static/js/2.ed654993.chunk.js"
+  },
+  {
+    "revision": "ef7f0128066dad638e14e7838f835a83",
+    "url": "/basic_blog-front/static/js/2.ed654993.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0929379f8afa5591b663",
+    "url": "/basic_blog-front/static/js/main.04dfc366.chunk.js"
+  },
+  {
+    "revision": "1d06fab86ee2a5231664",
+    "url": "/basic_blog-front/static/js/runtime-main.4e4e2e63.js"
+  }
+]);
